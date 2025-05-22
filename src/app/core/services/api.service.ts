bcf1991a7f35008b0f35f1e0de5b160a60bccb84
Alexander Costa <alexcesar@alexcesar.com>
2025-05-22 20:42:32 -0300

    [Feature] Loader
    * Add a spinner to act like a loader
    * Refactoring code

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, signal, WritableSignal } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from "rxjs";
-import { catchError, map, tap } from "rxjs/operators";
+import { catchError, tap, finalize } from "rxjs/operators";
 import  { Machine } from "../models/machine.model";
 
 @Injectable({
@@ -14,57 +14,70 @@ export class ApiService {
   private _machines = signal<Machine[]>([]);
   public readonly machines: WritableSignal<Machine[]> = this._machines;
 
+  private _isLoading: WritableSignal<boolean> = signal(false);
+  public readonly isLoading = this._isLoading.asReadonly();
+
   constructor(private http: HttpClient) { }
   
   // get all machines
   getMachines(): Observable<Machine[]> {
+    this._isLoading.set(true);
     return this.http.get<Machine[]>(this.apiUrl).pipe(
       tap(data => {
         this._machines.set(data);
         console.log('API Service: Machines fetched and signal updated.', data);
       }),
-      catchError(this.handleError)
+      catchError(this.handleError),
+      finalize(() => this._isLoading.set(false))
     );
   }
 
   // get machine by id
   getMachineById(id: string): Observable<Machine> {
+    this._isLoading.set(true);
     const url = `${this.apiUrl}/${id}`;
     return this.http.get<Machine>(url).pipe(
-      catchError(this.handleError)
+      catchError(this.handleError),
+      finalize(() => this._isLoading.set(false))
     );
   }
 
   // add a new machine
   addMachine(machine: Machine): Observable<Machine> {
+    this._isLoading.set(true);
     return this.http.post<Machine>(this.apiUrl, machine).pipe(
       tap(newMachine => this._machines.update(
         machines => [...machines, newMachine])
       ),
-      catchError(this.handleError)
+      catchError(this.handleError),
+      finalize(() => this._isLoading.set(false))
     );
   }
 
   // update an existing machine
   updateMachine(id: string, machine: Machine): Observable<Machine> {
+    this._isLoading.set(true);
     const url = `${this.apiUrl}/${id}`;
     return this.http.put<Machine>(url, machine).pipe(
       tap(updateMachine => {
         this._machines.update(machines => machines.map(m => m.id === updateMachine.id ? updateMachine : m));
       }),
-      catchError(this.handleError)
+      catchError(this.handleError),
+      finalize(() => this._isLoading.set(false))
     );
   }
 
   // delete an existing machine
   deleteMachine(id: string): Observable<void> {
+    this._isLoading.set(true);
     const url = `${this.apiUrl}/${id}`;
     return this.http.delete<void>(url).pipe(
       tap(() => { 
           this._machines.update(machines => machines.filter(m => m.id !== id))
         }
       ),
-      catchError(this.handleError)
+      catchError(this.handleError),
+      finalize(() => this._isLoading.set(false))
     );
   }
 
