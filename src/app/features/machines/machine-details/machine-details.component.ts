fcc1b26c3f18a7189492bff7d94a8f76b28fa7b9
Alexander Costa <alexcesar@alexcesar.com>
2025-05-22 15:29:46 -0300

    Machine CRUD structure

---
+++ src/app/features/machines/machine-details/machine-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-machine-details',
+  imports: [],
+  templateUrl: './machine-details.component.html',
+  styleUrl: './machine-details.component.scss'
+})
+export class MachineDetailsComponent {
+
+}
---
+++ src/app/features/machines/machine-list/machine-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-machine-list',
+  imports: [],
+  templateUrl: './machine-list.component.html',
+  styleUrl: './machine-list.component.scss'
+})
+export class MachineListComponent {
+
+}
